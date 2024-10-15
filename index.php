83ae3a9e5f10d9fa32a577951fe4fd95ea8813f8
canhdt17 <tadongcanh2k4@gmail.com>
2024-10-15 17:44:00 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+ </head>
+ <body>
+    <form action="" method="post">
+      
+      <input type="text" name="ten_sp">
+      <input type="text" name="gia_sp">
+      <input type="text" name="mota">
+      <input type="submit" value="Thêm" name="them">
+    </form> 
+
+ </body>
+ </html>
